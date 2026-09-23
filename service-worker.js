33f4c6911bc70191a61660f30f00e5f119bacc2d
AndreasZHAW <hocn@zhaw.ch>
2026-09-23 10:45:40 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ChaosKalender Service Worker
 // WICHTIG: CACHE_NAME bei jedem Deployment hochzählen (v5, v6, ...),
 // sonst bleiben alte Dateien im Cache hängen und Updates kommen nicht an.
-const CACHE_NAME = 'chaoskalender-v13';
+const CACHE_NAME = 'chaoskalender-v14';
 const ASSETS = [
   './',
   './index.html',
